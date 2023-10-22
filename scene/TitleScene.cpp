ba9db6d18e545885a6fd61abcf9a49820935cf02
HayatoInoue114 <k022g0087@g.neec.ac.jp>
2023-10-23 03:52:37 +0900

    いったんエラー無い

--- scene/TitleScene.cpp
+++ scene/TitleScene.cpp
@@ -25,86 +25,86 @@ void TitleScene::Update() {
 }
 
 
-	//// 座標をコピーしてオフセット分ずらす
-	//viewProjection_.translation_ = Add(worldTransform_.translation_, offset);
-XINPUT_STATE joyState{};
-	if (Input::GetInstance()->GetJoystickState(0, joyState)) {
-		//// デッドゾーンの設定
-		SHORT rightThumbX = Input::GetInstance()->ApplyDeadzone(joyState.Gamepad.sThumbRX);
-		const float kRadian = 0.02f;
-		viewProjection_.rotation_.y += (float)rightThumbX / SHRT_MAX * kRadian;
-	}
-	if (Input::GetInstance()->GetJoystickState(0, joyState)) {
-		// デッドゾーンの設定
-		SHORT leftThumbX = Input::GetInstance()->ApplyDeadzone(joyState.Gamepad.sThumbLX);
-		SHORT leftThumbZ = Input::GetInstance()->ApplyDeadzone(joyState.Gamepad.sThumbLY);
-		//const float kRadian = 0.02f;
-		viewProjection_.translation_.x += (float)leftThumbX / SHRT_MAX * 0.5f;
-		viewProjection_.translation_.z += (float)leftThumbZ / SHRT_MAX * 0.5f;
-	}
-
-	// Keyboard
-	if (Input::GetInstance()->PressKey(DIK_LEFT)) {
-		const float speed = -0.1f;
-
-		Vector3 move = { speed,0,0 };
-
-		// 移動ベクトルをカメラの角度だけ回転
-		move = TransformNormal(move, viewProjection_.matView);
-
-		viewProjection_.translation_ = Add(viewProjection_.translation_, move);
-	}
-	if (Input::GetInstance()->PressKey(DIK_RIGHT)) {
-		const float speed = 0.1f;
-
-		Vector3 move = { speed,0,0 };
-
-		// 移動ベクトルをカメラの角度だけ回転
-		move = TransformNormal(move, viewProjection_.matView);
-
-		viewProjection_.translation_ = Add(viewProjection_.translation_, move);
-	}
-	if (Input::GetInstance()->PressKey(DIK_UP)) {
-		const float speed = 0.1f;
-
-		Vector3 move = { 0,0, speed };
-
-		// 移動ベクトルをカメラの角度だけ回転
-		move = TransformNormal(move, viewProjection_.matView);
-
-		viewProjection_.translation_ = Add(viewProjection_.translation_, move);
-	}
-	if (Input::GetInstance()->PressKey(DIK_DOWN)) {
-		const float speed = -0.1f;
-
-		Vector3 move = { 0,0, speed };
-
-		// 移動ベクトルをカメラの角度だけ回転
-		move = TransformNormal(move, viewProjection_.matView);
-
-		viewProjection_.translation_ = Add(viewProjection_.translation_, move);
-	}
-
-	// keyboard
-	if (Input::GetInstance()->PressKey(DIK_W)) {
-		viewProjection_.rotation_ = Add(viewProjection_.rotation_, { -0.01f,0,0 });
-	}
-	if (Input::GetInstance()->PressKey(DIK_A)) {
-		viewProjection_.rotation_ = Add(viewProjection_.rotation_, { 0,-0.01f,0 });
-	}
-	if (Input::GetInstance()->PressKey(DIK_S)) {
-		viewProjection_.rotation_ = Add(viewProjection_.rotation_, { 0.01f,0,0 });
-	}
-	if (Input::GetInstance()->PressKey(DIK_D)) {
-		viewProjection_.rotation_ = Add(viewProjection_.rotation_, { 0,0.01f,0 });
-	}
-
-	viewProjection_.UpdateViewMatrix();
-	viewProjection_.TransferMatrix();
-}
+//	//// 座標をコピーしてオフセット分ずらす
+//	//viewProjection_.translation_ = Add(worldTransform_.translation_, offset);
+//XINPUT_STATE joyState{};
+//	if (Input::GetInstance()->GetJoystickState(0, joyState)) {
+//		//// デッドゾーンの設定
+//		SHORT rightThumbX = Input::GetInstance()->ApplyDeadzone(joyState.Gamepad.sThumbRX);
+//		const float kRadian = 0.02f;
+//		viewProjection_.rotation_.y += (float)rightThumbX / SHRT_MAX * kRadian;
+//	}
+//	if (Input::GetInstance()->GetJoystickState(0, joyState)) {
+//		// デッドゾーンの設定
+//		SHORT leftThumbX = Input::GetInstance()->ApplyDeadzone(joyState.Gamepad.sThumbLX);
+//		SHORT leftThumbZ = Input::GetInstance()->ApplyDeadzone(joyState.Gamepad.sThumbLY);
+//		//const float kRadian = 0.02f;
+//		viewProjection_.translation_.x += (float)leftThumbX / SHRT_MAX * 0.5f;
+//		viewProjection_.translation_.z += (float)leftThumbZ / SHRT_MAX * 0.5f;
+//	}
+//
+//	// Keyboard
+//	if (Input::GetInstance()->PressKey(DIK_LEFT)) {
+//		const float speed = -0.1f;
+//
+//		Vector3 move = { speed,0,0 };
+//
+//		// 移動ベクトルをカメラの角度だけ回転
+//		move = TransformNormal(move, viewProjection_.matView);
+//
+//		viewProjection_.translation_ = Add(viewProjection_.translation_, move);
+//	}
+//	if (Input::GetInstance()->PressKey(DIK_RIGHT)) {
+//		const float speed = 0.1f;
+//
+//		Vector3 move = { speed,0,0 };
+//
+//		// 移動ベクトルをカメラの角度だけ回転
+//		move = TransformNormal(move, viewProjection_.matView);
+//
+//		viewProjection_.translation_ = Add(viewProjection_.translation_, move);
+//	}
+//	if (Input::GetInstance()->PressKey(DIK_UP)) {
+//		const float speed = 0.1f;
+//
+//		Vector3 move = { 0,0, speed };
+//
+//		// 移動ベクトルをカメラの角度だけ回転
+//		move = TransformNormal(move, viewProjection_.matView);
+//
+//		viewProjection_.translation_ = Add(viewProjection_.translation_, move);
+//	}
+//	if (Input::GetInstance()->PressKey(DIK_DOWN)) {
+//		const float speed = -0.1f;
+//
+//		Vector3 move = { 0,0, speed };
+//
+//		// 移動ベクトルをカメラの角度だけ回転
+//		move = TransformNormal(move, viewProjection_.matView);
+//
+//		viewProjection_.translation_ = Add(viewProjection_.translation_, move);
+//	}
+//
+//	// keyboard
+//	if (Input::GetInstance()->PressKey(DIK_W)) {
+//		viewProjection_.rotation_ = Add(viewProjection_.rotation_, { -0.01f,0,0 });
+//	}
+//	if (Input::GetInstance()->PressKey(DIK_A)) {
+//		viewProjection_.rotation_ = Add(viewProjection_.rotation_, { 0,-0.01f,0 });
+//	}
+//	if (Input::GetInstance()->PressKey(DIK_S)) {
+//		viewProjection_.rotation_ = Add(viewProjection_.rotation_, { 0.01f,0,0 });
+//	}
+//	if (Input::GetInstance()->PressKey(DIK_D)) {
+//		viewProjection_.rotation_ = Add(viewProjection_.rotation_, { 0,0.01f,0 });
+//	}
+//
+//	viewProjection_.UpdateViewMatrix();
+//	viewProjection_.TransferMatrix();
+//}
 
 void TitleScene::Draw() {
-	titleName_->Draw(worldTransform_, 0, kBlendModeNone);
+	titleName_->Draw(worldTransform_, 0, kBlendModeNone,{1,1,1,1});
 }
 
 void TitleScene::Finalize() {
