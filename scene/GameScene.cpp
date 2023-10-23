982b5ada2babd628f4db15613b2d6b9a8aa87b15
k022g0087@g.neec.ac.jp <k022g0087@g.neec.ac.jp>
2023-10-23 12:12:38 +0900

    a

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -92,15 +92,15 @@ void GameScene::Update() {
 	skydome_->Update();
 
 	//FPSを120に固定する処理///////////////////////////////////////////////////
-	auto startTime = std::chrono::high_resolution_clock::now();
+	//auto startTime = std::chrono::high_resolution_clock::now();
 
-	auto endTime = std::chrono::high_resolution_clock::now();
-	std::chrono::duration<double> frameTime = endTime - startTime;
+	//auto endTime = std::chrono::high_resolution_clock::now();
+	//std::chrono::duration<double> frameTime = endTime - startTime;
 
-	// もしフレーム時間が目標のフレーム時間よりも短い場合、スリープでウェイトを入れる
-	if (frameTime.count() < targetFrameTime) {
-		std::this_thread::sleep_for(std::chrono::duration<double>(targetFrameTime - frameTime.count()));
-	}
+	//// もしフレーム時間が目標のフレーム時間よりも短い場合、スリープでウェイトを入れる
+	//if (frameTime.count() < targetFrameTime) {
+	//	std::this_thread::sleep_for(std::chrono::duration<double>(targetFrameTime - frameTime.count()));
+	//}
 	//////////////////////////////////////////////////////////////////////////
 
 	if (input_->TriggerKey(DIK_SPACE)) {
